1cb08c7576dcbf698fdc4eb145b602e9ced4e8d1
TUSHAR JADHAV <145747304+Tushar0-02@users.noreply.github.com>
2025-11-01 15:53:49 +0530

    Create riya.js

---
+++ riya.js
@@ -0,0 +1,59 @@
+const team = [
+  {
+    name: "Tushar Jadhav",
+    role: "Founder",
+    image: "tushar.jpg",
+
+  },
+  {
+    name: "Riya Kavathe",
+    role: "CEO",
+    image: "riya.jpg",
+
+  },
+  {
+    name: "Siddhesh Sulekar",
+    role: "COO",
+    image: "sidhesh.jpg",
+
+  },
+  {
+    name: "Aditya Patil",
+    role: "Fronted Devolper",
+    image: "aditya.jpg",
+
+  },
+  {
+    name: "Tnvi Bane",
+    role: "Backend Engineer",
+    image: "tnvi.jpg",
+
+
+  },
+  {
+    name: "Siddhi Potdar",
+    role: "UI/UX Designer",
+    image: "siddhi.jpg",
+
+  },
+  {
+    name: "Swaroop Patil",
+    role: "Marketing Head",
+    image: "swaroop.jpeg",
+
+  }
+];
+
+const container = document.getElementById("team");
+
+team.forEach(member => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+    <img src="${member.image}" alt="${member.name}" />
+    <h3>${member.name}</h3>
+    <h4>${member.role}</h4>
+    
+  `;
+  container.appendChild(card);
+});
